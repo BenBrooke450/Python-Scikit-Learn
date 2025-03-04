5c05afd478bdfcae953e5444d84d830c1606d2dd
BenBrooke450 <bensir@mail.com>
2025-03-04 21:47:55 +0100

    Basic

---
+++ Scikit-Lean Practice/tester.py
@@ -0,0 +1,5 @@
+from sklearn import datasets  # Import a dataset module from scikit-learn
+
+iris = datasets.load_iris() # Load the Iris flower dataset
+
+print(iris.data) # Print the data of the loaded Iris dataset
